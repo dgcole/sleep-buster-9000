72dd0ef9eab670a2c63b88b58515471881f9045a
dcole <dyllancole@outlook.com>
2020-01-18 13:20:30 -0500

    Implement buttons

--- src/main.cpp
+++ src/main.cpp
@@ -5,14 +5,17 @@
 // Max length of scrolling info string
 #define SCROLL_LENGTH 64
 
-// How frequently (measured in loop() calls) to scroll the text.
-#define SCROLL_FREQ 8
+// How frequently (measured in loop() calls) to scroll the text
+#define SCROLL_FREQ 16
 
-// How frequently (measured in loop() calls) to blink selections.
+// How frequently (measured in loop() calls) to blink selections
 #define BLINK_FREQ 8
 
 // Length of LCD (characters)
 #define LCD_LENGTH 16
+
+// Delay between loops (milliseconds)
+#define LOOP_DELAY 50
 
 // Pins for LCD
 #define LCD_RS  4
@@ -22,10 +25,13 @@
 #define LCD_D6  12
 #define LCD_D7  7
 
+// Button Pins
+#define LEFT_BUTTON_PIN     A1
+#define MIDDLE_BUTTON_PIN   A2
+#define RIGHT_BUTTON_PIN    A3
+#define SNOOZE_BUTTON_PIN   A6
 
-const char daysOfTheWeek[7] = {'S', 'M', 'T', 'W', 'R', 'F', 'S'};
-
-enum state {
+enum Page {
     STANDBY,
     SETTING_TIME,
     SETTING_DATE,
@@ -33,10 +39,18 @@ enum state {
     SETTING_ALARM_DAYS
 };
 
+enum Press {
+    NO_PRESS,
+    SHORT_PRESS,
+    LONG_PRESS
+};
+
+const char daysOfTheWeek[7] = {'S', 'M', 'T', 'W', 'R', 'F', 'S'};
+
 LiquidCrystal lcd(LCD_RS, LCD_E, LCD_D4, LCD_D5, LCD_D6, LCD_D7);
 
 // Current state for state machine in loop()
-state currState = STANDBY;
+Page currState = STANDBY;
 
 // Scrolling text on bottom row of 16x2 LCD
 char scroll[SCROLL_LENGTH] = "";
@@ -48,7 +62,7 @@ RTC_PCF8523 rtc;
 DateTime alarm;
 
 // Alarm set days
-bool alarmDays[7] = {true, true, true, true, true, true, true};
+bool alarmDays[7] = {true, false, true, false, true, false, true};
 
 // Represents selection number when setting day / time / alarm time / alarm days.
 uint8_t selection = 0;
@@ -113,41 +127,102 @@ void setScroll(char *str) {
     pos = strlen(str);
 
     char *end = &str[pos];
-    for (int i = 0; i < 7; i++) {
-        if (alarmDays[i] && ((blink % BLINK_FREQ) != 0)) {
-            *(end++) = daysOfTheWeek[i];
+    for (uint8_t i = 0; i < 7; i++) {
+        if (alarmDays[i]) {
+            if ((blink % (2 * BLINK_FREQ)) > BLINK_FREQ) {
+                *(end++) = daysOfTheWeek[i];
+            } else {
+                *(end++) = ' ';
+            }
         } else {
-            *(end++) = ' ';
+            *(end++) = daysOfTheWeek[i];
         }
     }
     *end++ = ' ';
     *end = '\0';
 }
 
-void setup() {
-    Serial.begin(57600);
-    while (!Serial) { //wait for
-        delay(1);
+Press getButtonPressState(uint8_t pin) {
+    static uint8_t downFor[8];
+
+    uint8_t index = pin - A0;
+    if (digitalRead(pin)) {
+        downFor[index]++;
+        // Goal is to only report press once. If you hold it down for longer
+        // than required, it should have no effect.
+        if (downFor[index] == (250 / LOOP_DELAY)) {
+            return LONG_PRESS;
+        }
+    } else {
+        uint8_t last = downFor[index];
+        downFor[index] = 0;
+        if ((last > 0) && (last < (250 / LOOP_DELAY))) {
+            return SHORT_PRESS;
+        }
+        return NO_PRESS;
     }
-    Serial.print("Wire Initialized!");
+}
+
+void setup() {
+    pinMode(LEFT_BUTTON_PIN, INPUT);
+    pinMode(MIDDLE_BUTTON_PIN, INPUT);
+    pinMode(RIGHT_BUTTON_PIN, INPUT);
+    pinMode(SNOOZE_BUTTON_PIN, INPUT);
 
     if (!rtc.begin()) {
-        Serial.println("Couldn't find RTC");
         exit(0);
     }
 
     if (!rtc.initialized()) {
-        Serial.println("RTC is NOT running!");
+        exit(0);
     }
 
     lcd.begin(16, 2);
-    setScroll(scroll);
 }
 
 void loop() {
-    // State Transitions
+    Press lButton = getButtonPressState(LEFT_BUTTON_PIN);
+    Press mButton = getButtonPressState(MIDDLE_BUTTON_PIN);
+    Press rButton = getButtonPressState(RIGHT_BUTTON_PIN);
+    Press sButton = getButtonPressState(SNOOZE_BUTTON_PIN);
 
-    // State Machine
+    // Update selection
+    if (mButton == SHORT_PRESS) {
+        uint8_t selectionMax = 0;
+        switch(currState) {
+            case SETTING_ALARM_DAYS:
+                selectionMax = 5;
+                break;
+            case SETTING_TIME:
+            case SETTING_DATE:
+            case SETTING_ALARM:
+            default:
+                selectionMax = 2;
+                break;
+        }
+
+        if (selection < selectionMax) {
+            selection++;
+        } else {
+            selection = 0;
+        }
+    } else if (mButton == LONG_PRESS) { // Switch Page
+        switch(currState) {
+            case STANDBY:
+            case SETTING_TIME:
+            case SETTING_DATE:
+            case SETTING_ALARM:
+                currState = (Page) (((uint8_t) currState) + 1);
+                break;
+            case SETTING_ALARM_DAYS:
+            default:
+                currState = STANDBY;
+                break;
+        }
+        selection = 0; // Reset selection when switching page.
+    }
+
+    // Page State Machine
     switch (currState) {
         case STANDBY: {
             setScroll(scroll);
@@ -158,14 +233,51 @@ void loop() {
             break;
         }
         case SETTING_TIME: {
+            if ((lButton != NO_PRESS) || (rButton != NO_PRESS)) {
+                int8_t dir = (rButton != NO_PRESS) -(lButton != NO_PRESS);
+
+                switch(selection) {
+                    case 0:
+                        rtc.adjust(DateTime(0, 0, 0, dir, 0, 0));
+                        break;
+                    case 1:
+                        rtc.adjust(DateTime(0, 0, 0, 0, dir, 0));
+                        break;
+                    case 2:
+                        rtc.adjust(DateTime(0, 0, 0, 0, 0, dir));
+                        break;
+                    default:
+                        break;
+                }
+            }
+
             lcd.clear();
             drawCenteredTime(rtc.now(), 0);
-            drawCentered("SET TIME", 1);
+            drawCentered("TIME", 1);
 
-            lcd.setCursor(4 + selection * 3, 0);
+            lcd.setCursor(5 + selection * 3, 0);
             lcd.blink();
+            break;
         }
         case SETTING_DATE: {
+            if ((lButton != NO_PRESS) || (rButton != NO_PRESS)) {
+                int8_t dir = (rButton != NO_PRESS) -(lButton != NO_PRESS);
+
+                switch(selection) {
+                    case 0:
+                        rtc.adjust(DateTime(0, dir, 0, 0, 0, 0));
+                        break;
+                    case 1:
+                        rtc.adjust(DateTime(0, 0, dir, 0, 0, 0));
+                        break;
+                    case 2:
+                        rtc.adjust(DateTime(dir, 0, 0, 0, 0, 0));
+                        break;
+                    default:
+                        break;
+                }
+            }
+
             DateTime now = rtc.now();
             char buf[11];
 
@@ -173,32 +285,71 @@ void loop() {
 
             lcd.clear();
             drawCentered(buf, 0);
-            drawCentered("SET DATE", 1);
+            drawCentered("DATE", 1);
 
-            lcd.setCursor(4 + selection * 3, 0);
+            if (selection < 2) {
+                lcd.setCursor(4 + selection * 3, 0);
+            } else {
+                lcd.setCursor(12, 0);
+            }
             lcd.blink();
+            break;
         }
         case SETTING_ALARM: {
+            if ((lButton != NO_PRESS) || (rButton != NO_PRESS)) {
+                int8_t dir = (rButton != NO_PRESS) -(lButton != NO_PRESS);
+
+                switch(selection) {
+                    case 0:
+                        alarm = DateTime(alarm.year(), alarm.month(), alarm.day(), alarm.hour() + 1, alarm.minute(), alarm.second());
+                        break;
+                    case 1:
+                        alarm = DateTime(alarm.year(), alarm.month(), alarm.day(), alarm.hour(), alarm.minute() + 1, alarm.second());
+                        break;
+                    case 2:
+                        alarm = DateTime(alarm.year(), alarm.month(), alarm.day(), alarm.hour(), alarm.minute(), alarm.second() + 1);
+                        break;
+                    default:
+                        break;
+                }
+            }
+
             lcd.clear();
             drawCenteredTime(alarm, 0);
-            drawCentered("SET TIME", 1);
+            drawCentered("ALARM TIME", 1);
 
-            lcd.setCursor(4 + selection * 3, 0);
+            lcd.setCursor(5 + selection * 3, 0);
             lcd.blink();
+            break;
         }
         case SETTING_ALARM_DAYS: {
+            if ((lButton != NO_PRESS) || (rButton != NO_PRESS)) {
+                bool on = (rButton != NO_PRESS) && (lButton == NO_PRESS);
+                alarmDays[selection] = on;
+            }
+
+            char dayBuf[14] = "             ";
+            for (uint8_t i = 0; i < 7; i++) {
+                if (alarmDays[i]) {
+                    if ((blink % (2 * BLINK_FREQ)) > BLINK_FREQ) {
+                        dayBuf[i * 2] = daysOfTheWeek[i];
+                    }
+                } else {
+                    dayBuf[i * 2] = daysOfTheWeek[i];
+                }
+            }
             lcd.clear();
-            //TODO; Make selected days blink.
-            drawCentered("S M T W R F S", 0);
-            drawCentered("SET ALARM DAYS", 1);
+            drawCentered(dayBuf, 0);
+            drawCentered("ALARM DAYS", 1);
 
             lcd.setCursor(2 + selection * 2, 0);
             lcd.blink();
+            break;
         }
         default:
             break;
     }
 
     blink++;
-    delay(100);
+    delay(LOOP_DELAY);
 }
